f3285f78d85e3612d0ca80dc99c789f0017fffab
Ananya <ananyatandel02@gmail.com>
2020-07-16 21:41:09 -0600

    Initial Commit for Final Project

---
+++ Green/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Final Project
+//
+//  Created by Ananya Tandel on 7/14/20.
+//  Copyright © 2020 Ananya Tandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    // Checkbox Button
+//    @IBOutlet weak var btn: UIButton!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    @IBAction func btnAction( sender: UIButton) {
+//        if sender.isSelected == true {
+//            btn.setImage(UIImage(named: "checked"), for: .normal)
+//            sender.isSelected = false
+//        } else {
+//            btn.setImage(UIImage(named: "unchecked"), for: .normal)
+//            sender.isSelected = true
+//        }
+    // hopefully this works!
+    var btn = "Hello"
+    }
+
+
